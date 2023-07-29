fb15b6187bda1c03fbc4dfe0dc44213d09a6b0bb
AbhithaHeshan <abeethaheshan0001@gmail.com>
2023-07-29 20:36:05 +0530

    variables

---
+++ variables/1_start.php
@@ -0,0 +1,30 @@
+<?php
+   
+   // syntax
+   // $variable_name = value;
+
+   //Integer
+   $num = 20;
+   echo $num;
+
+   //String
+   $name = "Amal";
+   echo $name;
+
+   // Boolean variable
+   $isStudent = true;
+
+   // Array variable
+   $fruits = array("apple", "banana", "orange");
+
+   // Float variable
+   $pi = 3.14;
+
+   // Null variable
+   $nothing = null;
+
+   // Variable interpolation
+   $city = "New York";
+   echo "I live in $city."; // Output: I live in New York.
+
+?>
